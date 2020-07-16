5cca38251d4771b0d3699a262ab36bbf21afd83c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-16 14:05:11 +0200

    Refaktoring

--- Altkom.WPFMVVM.Models/ObservableCollectionExtensions.cs
+++ Altkom.WPFMVVM.Models/ObservableCollectionExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
-namespace Altkom.WPFMVVM.ViewModels
+namespace Altkom.WPFMVVM.Models
 {
     public static class ObservableCollectionExtensions
     {
