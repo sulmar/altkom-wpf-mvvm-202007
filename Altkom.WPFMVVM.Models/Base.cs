8442ef418f850019bae41dce11ce1804fffe28b4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-16 14:03:38 +0200

    Refaktoring

--- Altkom.WPFMVVM.Models/Base.cs
+++ Altkom.WPFMVVM.Models/Base.cs
@@ -21,21 +21,4 @@ namespace Altkom.WPFMVVM.Models
 
 
     }
-
-    public static class INotifyPropertyChangedExtensions
-    {
-        public static void ExecuteOnPropertyChanged(this INotifyPropertyChanged item, string propname, System.Action action)
-        {
-           item.PropertyChanged += (s, e) =>
-           {
-               if (e.PropertyName == propname)
-               {
-                   action?.Invoke();
-               };
-
-               return;
-           };
-
-        }
-    }
 }
---
+++ Altkom.WPFMVVM.Models/INotifyPropertyChangedExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Altkom.WPFMVVM.Models
+{
+    public static class INotifyPropertyChangedExtensions
+    {
+        public static void ExecuteOnPropertyChanged(this INotifyPropertyChanged item, string propname, System.Action action)
+        {
+           item.PropertyChanged += (s, e) =>
+           {
+               if (e.PropertyName == propname)
+               {
+                   action?.Invoke();
+               };
+
+               return;
+           };
+
+        }
+    }
+}
